06f7450aa78649e7a451feabab3bc9cb9f4ec30b
TheLordA <45141652+TheLordA@users.noreply.github.com>
2020-06-16 22:20:23 +0100

    Add Home Screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,14 +1,84 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { SafeAreaView, StyleSheet, Platform } from "react-native";
+import Swiper from "react-native-deck-swiper";
+import { Card } from "../components/Card";
+import { HomeScreenPics } from "../constants/Pics";
 
+// tempo definition of colors , should be donne in constants/colors.js
+const colors = {
+	red: "#EC2379",
+	blue: "#0070FF",
+	gray: "#777777",
+	white: "#ffffff",
+	black: "#000000",
+};
 class HomeScreen extends React.Component {
 	render() {
 		return (
-			<View>
-				<Text>Home Screen</Text>
-			</View>
+			<SafeAreaView style={styles.container}>
+				<Swiper
+					cards={HomeScreenPics}
+					renderCard={Card}
+					infinite // keep looping cards infinitely
+					backgroundColor={"transparent"}
+					cardHorizontalMargin={0}
+					stackSize={4} // number of cards shown in background
+					useViewOverflow={Platform.OS === "ios"}
+					animateOverlayLabelsOpacity
+					animateCardOpacity
+					disableTopSwipe
+					disableBottomSwipe
+					overlayLabels={{
+						left: {
+							title: "NOPE",
+							style: {
+								label: {
+									backgroundColor: colors.red,
+									borderColor: colors.red,
+									color: colors.white,
+									borderWidth: 1,
+									fontSize: 24,
+								},
+								wrapper: {
+									flexDirection: "column",
+									alignItems: "flex-end",
+									justifyContent: "flex-start",
+									marginTop: 20,
+									marginLeft: -20,
+								},
+							},
+						},
+						right: {
+							title: "LIKE",
+							style: {
+								label: {
+									backgroundColor: colors.blue,
+									borderColor: colors.blue,
+									color: colors.white,
+									borderWidth: 1,
+									fontSize: 24,
+								},
+								wrapper: {
+									flexDirection: "column",
+									alignItems: "flex-start",
+									justifyContent: "flex-start",
+									marginTop: 20,
+									marginLeft: 20,
+								},
+							},
+						},
+					}}
+				/>
+			</SafeAreaView>
 		);
 	}
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: "transparent",
+	},
+});
 
 export default HomeScreen;
---
+++ utils/shuffleArray.js
@@ -0,0 +1,9 @@
+// This makes sure our array is randomized every time.
+// found at https://stackoverflow.com/a/46545530/6141587
+const shuffleArray = (array) =>
+	array
+		.map((a) => ({ sort: Math.random(), value: a }))
+		.sort((a, b) => a.sort - b.sort)
+		.map((a) => a.value);
+
+export default shuffleArray;
